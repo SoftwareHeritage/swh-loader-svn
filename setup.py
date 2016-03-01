8c80cab0a5cdb3f6b089f68953e330853ddd849c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-01 16:00:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+
+setup(
+    name='swh.loader.svn',
+    description='Software Heritage Loader SVN',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DLDSVN',
+    packages=[],  # packages's modules
+    scripts=[],   # scripts to package
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)

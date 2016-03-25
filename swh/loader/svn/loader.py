dd0198806b8deecd2e7296761718536ce8763618
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-03-25 10:29:43 +0100

    Refactor - Add abstraction around svn repository

--- swh/loader/svn/loader.py
+++ swh/loader/svn/loader.py
@@ -4,226 +4,12 @@
 # See top-level LICENSE file for more information
 
 import datetime
-import os
-import pysvn
-import tempfile
-import subprocess
-import sys
-import traceback
-
-from contextlib import contextmanager
 
 from swh.core import hashutil
-from swh.loader.svn import libloader
 from swh.model import git
 from swh.model.git import GitType
 
-
-@contextmanager
-def cwd(path):
-    """Contextually change the working directory to do thy bidding.
-    Then gets back to the original location.
-
-    """
-    prev_cwd = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(prev_cwd)
-
-
-def repo_uuid(local_path):
-    with cwd(local_path):
-        cmd = 'svn info | grep UUID | cut -f2 -d:'
-        uuid = subprocess.check_output(cmd, shell=True)
-        return uuid.strip().decode('utf-8')
-
-
-def init_repo(remote_repo_url, destination_path=None):
-    """Initialize a repository without any action on disk.
-
-    Args:
-        remote_repo_url: The remote svn url
-        destination_path: The optional local parent folder to checkout the
-        repository to
-
-    Returns:
-        Dictionary with the following keys:
-            - client: client instance to manipulate the repository
-            - remote_url: remote url (same as input)
-            - local_url: local url which has been computed
-
-    """
-    name = os.path.basename(remote_repo_url)
-    if destination_path:
-        os.makedirs(destination_path, exist_ok=True)
-        local_dirname = destination_path
-    else:
-        local_dirname = tempfile.mkdtemp(suffix='swh.loader',
-                                         prefix='tmp.',
-                                         dir='/tmp')
-
-    local_repo_url = os.path.join(local_dirname, name)
-
-    client = pysvn.Client()
-
-    return {'client': client,
-            'remote_url': remote_repo_url,
-            'local_url': local_repo_url}
-
-
-def fork(repo):
-    """Checkout remote repository remote_repo_url to local working copy
-    destination_path.
-
-    Args:
-        remote_repo_url: The remote svn url
-        destination_path: The optional local parent folder to checkout the
-        repository to
-
-    Returns:
-        Dictionary with the following keys:
-            - client: client instance to manipulate the repository
-            - uuid: the repository's uuid
-            - remote_url: remote url (same as input)
-            - local_url: local url which has been computed
-
-    """
-    repo['client'].checkout(repo['remote_url'], repo['local_url'])
-
-    uuid = repo_uuid(repo['local_url'])
-
-    return {'client': repo['client'],
-            'uuid': uuid,
-            'remote_url': repo['remote_url'],
-            'local_url': repo['local_url']}
-
-
-def checkout(repo, revision):
-    """Checkout repository repo at revision.
-
-    Args:
-        repo: the repository instance
-        revision: the revision number to checkout the repo to.
-
-    """
-    repo['client'].checkout(
-        repo['remote_url'],
-        repo['local_url'],
-        revision=pysvn.Revision(pysvn.opt_revision_kind.number, revision))
-
-
-def svn_read_current_revision_at_path(repo):
-    """Retrieve current revision at local path."""
-    head_rev = pysvn.Revision(pysvn.opt_revision_kind.head)
-    info = repo['client'].info2(repo['local_url'],
-                                revision=head_rev,
-                                recurse=False)
-    return info[0][1]['rev'].number
-
-
-def svn_read_origin_revision_from_url(repo):
-    """Determine the first revision number from which the url appeared.
-
-    This is only extracted from the svn repository.
-
-    """
-    return repo['client'].log(repo['remote_url'])[-1].data.get(
-        'revision').number
-
-
-def svn_logs(repo):
-    """Return the logs of the repository between the revision start and end of such
-    repository.
-
-    """
-    return repo['client'].log(
-        url_or_path=repo['local_url'],
-        revision_start=pysvn.Revision(pysvn.opt_revision_kind.number,
-                                      repo['revision_start']),
-        revision_end=pysvn.Revision(pysvn.opt_revision_kind.number,
-                                    repo['revision_end']))
-
-
-def swh_check_for_previous_revision(repo, origin_id):
-    """Look for possible existing revision.
-
-    Return:
-        The previous svn revision known if found, None otherwise.
-
-    """
-    storage = repo['storage']
-    occ = storage.occurrence_get(origin_id)
-    if occ:
-        revision_id = occ[0]['target']
-        revisions = storage.revision_get([revision_id])
-        parents = {}
-        for rev, pparents in storage.revision_shortlog([revision_id], limit=1):
-            parents[rev] = pparents
-
-        if revisions:
-            rev = revisions[0]
-            svn_revision = rev['metadata']['extra_headers']['svn_revision']
-            return svn_revision, parents[revision_id]
-
-    return None, None
-
-
-def read_log_entries(repo):
-    """Read the logs entries from the repository repo.
-
-    Args
-        repo: the repository instance
-
-    Returns
-        tuple of revisions and logs.
-        revisions: list of revisions in order
-        logs: Dictionary with key revision number and value the log entry.
-              The log entry is a dictionary with the following keys:
-                 - author_date: date of the commit
-                 - author_name: name of the author
-                 - message: commit message
-
-    """
-    logs = {}
-    revisions = []
-    for log in svn_logs(repo):
-        rev = log.revision.number
-        revisions.append(rev)
-        logs[rev] = {'author_date': log.date,
-                     'author_name': log.author,
-                     'message': log.message}
-
-    return revisions, logs
-
-
-def read_svn_revisions(repo):
-    """Compute tree for each revision from last known revision to
-    latest_revision.
-
-    """
-    revisions = repo['revisions']
-    logs = repo['logs']
-    l = len(revisions)
-    for i, rev in enumerate(revisions):
-        # checkout to the revision rev
-        checkout(repo, revision=rev)
-
-        # compute git commit
-        objects_per_path = git.walk_and_compute_sha1_from_directory(
-            repo['local_url'].encode('utf-8'),
-            dir_ok_fn=lambda dirpath: b'.svn' not in dirpath)
-
-        commit = logs[rev]
-
-        nextrev_index = i+1
-        if nextrev_index < l:
-            nextrev = revisions[nextrev_index]
-        else:
-            nextrev = None
-
-        yield rev, nextrev, commit, objects_per_path
+from swh.loader.svn import libloader, svn
 
 
 def build_swh_revision(repo_uuid, commit, rev, dir_id, parents):
@@ -313,52 +99,33 @@ class SvnLoader(libloader.SWHLoader):
             - stderr: optional when status is True, mandatory otherwise
 
         """
-        repo = init_repo(svn_url, destination_path)
-        repo['storage'] = self.storage
-        revision_start, revision_parents = swh_check_for_previous_revision(
-            repo, origin['id'])
+        svnrepo = svn.SvnRepo(svn_url, origin['id'], self.storage,
+                              destination_path)
+        revision_start, revision_parents = svnrepo.swh_previous_revision_and_parents()  # noqa
 
-        repo = fork(repo)
-
-        # 2. retrieve current svn revision
+        # initial checkout
+        svnrepo.fork()
 
         if not revision_start:
-            revision_start = svn_read_origin_revision_from_url(repo)
+            revision_start = svnrepo.initial_revision()
 
-        revision_end = svn_read_current_revision_at_path(repo)
+        revision_end = svnrepo.current_revision()
 
-        repo.update({'revision_start': revision_start,
-                     'revision_end': revision_end})
-
-        self.log.debug('repo: %s' % repo)
+        self.log.debug('svnrepo: %s' % svnrepo)
 
         if revision_start == revision_end and revision_start is not 1:
             self.log.info('%s@%s already injected.' % (svn_url, revision_end))
             return {'status': True}
-
-        revisions, logs = read_log_entries(repo)
-        repo.update({
-            'revisions': revisions,
-            'logs': logs
-        })
-
-        self.log.debug('revisions: %s, ..., %s' % (revisions[0],
-                                                   revisions[-1]))
-        self.log.debug('logs: %s, ..., %s' % (logs[revision_start],
-                                              logs[revision_end]))
-
-        repo_uuid = repo['uuid']
 
         if revision_start == 1:
             parents = {revision_start: []}  # no parents for initial revision
         else:
             parents = {revision_start: revision_parents}
 
-        self.log.debug('parents: %s' % parents)
-
         swh_revisions = []
         # for each revision
-        for rev, nextrev, commit, objects_per_path in read_svn_revisions(repo):
+        for rev, nextrev, commit, objects_per_path in svnrepo.swh_hash_data_per_revision(  # noqa
+                revision_start, revision_end):
             self.log.debug('rev: %s, nextrev: %s' % (rev, nextrev))
 
             objects_per_type = {
@@ -373,7 +140,8 @@ class SvnLoader(libloader.SWHLoader):
 
             dir_id = objects_per_path[git.ROOT_TREE_KEY][0]['sha1_git']
             self.log.debug('tree: %s' % hashutil.hash_to_hex(dir_id))
-            swh_revision = build_swh_revision(repo_uuid, commit, rev, dir_id,
+            swh_revision = build_swh_revision(svnrepo.uuid, commit, rev,
+                                              dir_id,
                                               parents[rev])
             swh_revision['id'] = git.compute_revision_sha1_git(swh_revision)
             if nextrev:
@@ -444,14 +212,14 @@ class SvnLoaderWithHistory(SvnLoader):
 
         fetch_history_id = self.open_fetch_history(origin['id'])
 
-        try:
-            result = super().process(svn_url, origin, destination_path)
-        except:
-            e_info = sys.exc_info()
-            self.log.error('Problem during svn load for repo %s - %s' % (
-                svn_url, e_info[1]))
-            result = {'status': False, 'stderr': 'reason:%s\ntrace:%s' % (
-                    e_info[1],
-                    ''.join(traceback.format_tb(e_info[2])))}
+        # try:
+        result = super().process(svn_url, origin, destination_path)
+        # except:
+        #     e_info = sys.exc_info()
+        #     self.log.error('Problem during svn load for repo %s - %s' % (
+        #         svn_url, e_info[1]))
+        #     result = {'status': False, 'stderr': 'reason:%s\ntrace:%s' % (
+        #             e_info[1],
+        #             ''.join(traceback.format_tb(e_info[2])))}
 
         self.close_fetch_history(fetch_history_id, result)
---
+++ swh/loader/svn/svn.py
@@ -0,0 +1,226 @@
+# Copyright (C) 2015  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import os
+import pysvn
+import tempfile
+import subprocess
+
+from contextlib import contextmanager
+
+from swh.model import git
+
+
+@contextmanager
+def cwd(path):
+    """Contextually change the working directory to do thy bidding.
+    Then gets back to the original location.
+
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+
+def init_repo(remote_repo_url, destination_path=None):
+    """Initialize a repository without any svn action on disk. There may be
+    temporary folder creation on disk as side effect (if destination_path is
+    not provided)
+
+    Args:
+        remote_repo_url: The remote svn url
+        destination_path: The optional local parent folder to checkout the
+        repository to.
+
+    Returns:
+        Dictionary with the following keys:
+            - client: client instance to manipulate the repository
+            - remote_url: remote url (same as input)
+            - local_url: local url which has been computed
+
+    """
+    name = os.path.basename(remote_repo_url)
+    if destination_path:
+        os.makedirs(destination_path, exist_ok=True)
+        local_dirname = destination_path
+    else:
+        local_dirname = tempfile.mkdtemp(suffix='.swh.loader',
+                                         prefix='tmp.',
+                                         dir='/tmp')
+
+    local_repo_url = os.path.join(local_dirname, name)
+
+    client = pysvn.Client()
+
+    return {'client': client,
+            'remote_url': remote_repo_url,
+            'local_url': local_repo_url}
+
+
+class SvnRepo():
+    """Swh representation of a svn repository.
+
+    """
+
+    def __init__(self, remote_url, origin_id, storage, local_url=None):
+        self.remote_url = remote_url
+        self.storage = storage
+        self.origin_id = origin_id
+
+        r = init_repo(remote_url, local_url)
+        self.client = r['client']
+        self.local_url = r['local_url']
+        self.uuid = None
+
+    def __str__(self):
+        return str({'remote_url': self.remote_url,
+                    'local_url': self.local_url,
+                    'uuid': self.uuid,
+                    'swh-origin': self.origin_id})
+
+    def read_uuid(self):
+        with cwd(self.local_url):
+            cmd = 'svn info | grep UUID | cut -f2 -d:'
+            uuid = subprocess.check_output(cmd, shell=True)
+            return uuid.strip().decode('utf-8')
+
+    def fork(self):
+        """Checkout remote repository to a local working copy.
+
+        This will also update the repository's uuid.
+
+        """
+        self.client.checkout(self.remote_url, self.local_url)
+        self.uuid = self.read_uuid()
+
+    def checkout(self, revision):
+        """Checkout repository repo at revision.
+
+        Args:
+            revision: the revision number to checkout the repo to.
+
+        """
+        self.client.checkout(
+            self.remote_url,
+            self.local_url,
+            revision=pysvn.Revision(pysvn.opt_revision_kind.number, revision))
+
+    def current_revision(self):
+        """Retrieve current revision of the repository's working copy.
+
+        """
+        head_rev = pysvn.Revision(pysvn.opt_revision_kind.head)
+        info = self.client.info2(self.local_url,
+                                 revision=head_rev,
+                                 recurse=False)
+        return info[0][1]['rev'].number
+
+    def initial_revision(self):
+        """Retrieve the initial revision from which the remote url appeared.
+        Note: This should always be 1 since we won't be dealing with in-depth
+        url.
+
+        """
+        return self.client.log(self.remote_url)[-1].data.get(
+            'revision').number
+
+    def logs(self, revision_start, revision_end):
+        """Return the revisions and logs of the repository between the revision start
+        and revision_end.
+
+        Args
+            repo: the repository instance
+
+        Returns
+            tuple of revisions and logs.
+            revisions: list of revisions in order
+            logs: Dictionary with key revision number and value the log entry.
+                  The log entry is a dictionary with the following keys:
+                     - author_date: date of the commit
+                     - author_name: name of the author
+                     - message: commit message
+
+        """
+        r0 = pysvn.Revision(pysvn.opt_revision_kind.number, revision_start)
+        r1 = pysvn.Revision(pysvn.opt_revision_kind.number, revision_end)
+        svn_logs = self.client.log(url_or_path=self.local_url,
+                                   revision_start=r0,
+                                   revision_end=r1)
+
+        logs = {}
+        revisions = []
+        for log in svn_logs:
+            rev = log.revision.number
+            revisions.append(rev)
+            logs[rev] = {'author_date': log.date,
+                         'author_name': log.author,
+                         'message': log.message}
+
+        return revisions, logs
+
+    def swh_previous_revision_and_parents(self):
+        """Look for possible existing revision.
+
+        Returns:
+            The tuple (previous svn revision known by swh and its parents) if
+            the svn revision is found, the tuple (None, None) otherwise.
+
+        """
+        storage = self.storage
+        occ = storage.occurrence_get(self.origin_id)
+        if occ:
+            revision_id = occ[0]['target']
+            revisions = storage.revision_get([revision_id])
+            parents = {}
+            for rev, pparents in storage.revision_shortlog([revision_id],
+                                                           limit=1):
+                parents[rev] = pparents
+
+            if revisions:
+                rev = revisions[0]
+                svn_revision = rev['metadata']['extra_headers']['svn_revision']
+                return svn_revision, parents[revision_id]
+
+        return None, None
+
+    def swh_hash_data_per_revision(self, start_revision, end_revision):
+        """Compute swh hash data per each revision between start_revision and
+        end_revision.
+
+        Args:
+            start_revision: starting revision
+            end_revision: ending revision
+
+        Yields:
+            tuple (rev, nextrev, commit, objects_per_path)
+            - rev: current revision
+            - nextrev: next revision
+            - commit: commit data (author, date, message) for such revision
+            - objects_per_path: dictionary of path, swh hash data with type
+
+        """
+        revisions, logs = self.logs(start_revision, end_revision)
+        l = len(revisions)
+        local_url = self.local_url.encode('utf-8')
+        for i, rev in enumerate(revisions):
+            # checkout to the revision rev
+            self.checkout(revision=rev)
+
+            # compute git commit
+            objects_per_path = git.walk_and_compute_sha1_from_directory(
+                local_url, dir_ok_fn=lambda dirpath: b'.svn' not in dirpath)
+
+            commit = logs[rev]
+
+            nextrev_index = i+1
+            if nextrev_index < l:
+                nextrev = revisions[nextrev_index]
+            else:
+                nextrev = None
+
+            yield rev, nextrev, commit, objects_per_path

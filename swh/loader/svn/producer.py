7568ec01eb5653df074f6b8f25f6176c4609d29c
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-04-04 15:38:38 +0200

    Improve producer to send multiple svn repos

--- swh/loader/svn/producer.py
+++ swh/loader/svn/producer.py
@@ -1,4 +1,11 @@
+# Copyright (C) 2015-2016  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 import click
+import sys
+
 
 task_name = 'swh.loader.svn.tasks.LoadSvnRepositoryTsk'
 
@@ -12,7 +19,13 @@ def main(svn_url, destination_path):
     from swh.scheduler.celery_backend.config import app
     from swh.loader.svn import tasks  # noqa
 
-    app.tasks[task_name].delay(svn_url, destination_path)
+    if svn_url:
+        app.tasks[task_name].delay(svn_url, destination_path)
+    else:
+        for svn_url in sys.stdin:
+            svn_url = svn_url.rstrip()
+            print(svn_url)
+            app.tasks[task_name].delay(svn_url, destination_path)
 
 
 if __name__ == '__main__':

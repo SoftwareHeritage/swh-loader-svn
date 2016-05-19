5ed3f97dd583b7d3244ad4f30e9cdcebf133b8b0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-05-19 15:58:14 +0200

    Add options to ignore empty folder
    Snag:
    - svn does checkout the empty folder (and it influences the sha1_git
    computation)
    - git svn does not by default checkout those (so different hashes)
    - or git svn does checkout those empty folder but with a placeholder
    filename (defaulting to .gitignore) and it's used for sha1_git
    computation.

--- bin/hashtree.py
+++ bin/hashtree.py
@@ -6,6 +6,7 @@
 # hash: 38f8d2c3a951f6b94007896d0981077e48bbd702
 
 import click
+import os
 
 from swh.model import git
 from swh.core import hashutil
@@ -14,9 +15,11 @@ from swh.core import hashutil
 @click.command()
 @click.option('--path', default='.',
               help='Optional path to hash.')
+@click.option('--ignore-empty-folder', is_flag=True,
+              help='Ignore empty folder.')
 @click.option('--ignore', multiple=True,
               help='Ignore pattern.')
-def hashtree(path, ignore=None):
+def hashtree(path, ignore_empty_folder=False, ignore=None):
     if isinstance(path, str):
         path = path.encode('utf-8')
 
@@ -25,12 +28,24 @@ def hashtree(path, ignore=None):
         for exc in ignore:
             patterns.append(exc.encode('utf-8'))
 
-        def dir_ok_fn(dirpath, patterns=patterns):
-            for pattern_to_ignore in patterns:
-                if pattern_to_ignore in dirpath:
+        if ignore_empty_folder:
+            def dir_ok_fn(dirpath, patterns=patterns):
+                if len(os.listdir(dirpath)) == 0:
                     return False
 
-            return True
+                for pattern_to_ignore in patterns:
+                    if pattern_to_ignore in dirpath:
+                        return False
+
+                return True
+
+        else:
+            def dir_ok_fn(dirpath, patterns=patterns):
+                for pattern_to_ignore in patterns:
+                    if pattern_to_ignore in dirpath:
+                        return False
+
+                return True
 
         objects = git.walk_and_compute_sha1_from_directory(
             path,

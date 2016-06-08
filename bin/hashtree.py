079f6556579d19c9e5222a0cec1775c921343d4f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-06-08 15:31:40 +0200

    Fix flag to actually work in all cases + use the new swh-model api

--- bin/hashtree.py
+++ bin/hashtree.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # Use sample:
-# hashtree.py --path . --ignore '.svn' --ignore '.git-svn'
+# hashtree.py --path . --ignore '.svn' --ignore '.git-svn' \
+#    --ignore-empty-folders
 # 38f8d2c3a951f6b94007896d0981077e48bbd702
 
 import click
@@ -14,11 +15,19 @@ from swh.core import hashutil
 @click.command()
 @click.option('--path', default='.',
               help='Optional path to hash.')
-@click.option('--ignore-empty-folder', is_flag=True,
+@click.option('--ignore-empty-folder', is_flag=True, default=False,
               help='Ignore empty folder.')
 @click.option('--ignore', multiple=True,
               help='Ignore pattern.')
-def hashtree(path, ignore_empty_folder=False, ignore=None):
+def hashtree(path, ignore_empty_folder, ignore=None):
+    if os.path.exists(path):
+        if not os.path.isdir(path):
+            print('%s should be a directory!' % path)
+            return
+    else:
+        print('%s should exist!' % path)
+        return
+
     if isinstance(path, str):
         path = path.encode('utf-8')
 
@@ -40,19 +49,21 @@ def hashtree(path, ignore_empty_folder=False, ignore=None):
             def dir_ok_fn(dirpath, patterns=patterns):
                 if not dir_ok_fn_basic(dirpath):
                     return False
-                if os.listdir(dirpath) == []:
-                    return False
-                return True
+                return os.listdir(dirpath) != []
         else:
             dir_ok_fn = dir_ok_fn_basic
-
-        objects = git.walk_and_compute_sha1_from_directory(
-            path,
-            dir_ok_fn=dir_ok_fn)
     else:
-        objects = git.walk_and_compute_sha1_from_directory(path)
+        if ignore_empty_folder:
+            def dir_ok_fn(dirpath):
+                return os.listdir(dirpath) != []
+        else:
+            dir_ok_fn = git.default_validation_dir
 
-    h = objects[git.ROOT_TREE_KEY][0]['sha1_git']
+    objects = git.walk_and_compute_sha1_from_directory_2(
+        path,
+        dir_ok_fn=dir_ok_fn)
+
+    h = objects[path]['checksums']['sha1_git']
 
     print(hashutil.hash_to_hex(h))
 

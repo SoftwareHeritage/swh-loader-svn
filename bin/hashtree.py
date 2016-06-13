0a601388423025609fb3cd7d980278ae182f4c77
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-06-13 12:07:21 +0200

    Refactor - Expose the hashtree function in utils module

--- bin/hashtree.py
+++ bin/hashtree.py
@@ -6,10 +6,10 @@
 # 38f8d2c3a951f6b94007896d0981077e48bbd702
 
 import click
-import os
 
-from swh.model import git
 from swh.core import hashutil
+
+from swh.loader.svn.utils import hashtree
 
 
 @click.command()
@@ -19,54 +19,12 @@ from swh.core import hashutil
               help='Ignore empty folder.')
 @click.option('--ignore', multiple=True,
               help='Ignore pattern.')
-def hashtree(path, ignore_empty_folder, ignore=None):
-    if os.path.exists(path):
-        if not os.path.isdir(path):
-            print('%s should be a directory!' % path)
-            return
-    else:
-        print('%s should exist!' % path)
-        return
+def main(path, ignore_empty_folder, ignore=None):
+    h = hashtree(path, ignore_empty_folder, ignore)
 
-    if isinstance(path, str):
-        path = path.encode('utf-8')
-
-    if ignore:
-        patterns = []
-        for exc in ignore:
-            patterns.append(exc.encode('utf-8'))
-
-        def dir_ok_fn_basic(dirpath, patterns=patterns):
-            dname = os.path.basename(dirpath)
-            for pattern_to_ignore in patterns:
-                if pattern_to_ignore == dname:
-                    return False
-                if (pattern_to_ignore + b'/') in dirpath:
-                    return False
-            return True
-
-        if ignore_empty_folder:
-            def dir_ok_fn(dirpath, patterns=patterns):
-                if not dir_ok_fn_basic(dirpath):
-                    return False
-                return os.listdir(dirpath) != []
-        else:
-            dir_ok_fn = dir_ok_fn_basic
-    else:
-        if ignore_empty_folder:
-            def dir_ok_fn(dirpath):
-                return os.listdir(dirpath) != []
-        else:
-            dir_ok_fn = git.default_validation_dir
-
-    objects = git.compute_hashes_from_directory(
-        path,
-        dir_ok_fn=dir_ok_fn)
-
-    h = objects[path]['checksums']['sha1_git']
-
-    print(hashutil.hash_to_hex(h))
+    if h:
+        print(hashutil.hash_to_hex(h['sha1_git']))
 
 
 if __name__ == '__main__':
-    hashtree()
+    main()
--- swh/loader/svn/utils.py
+++ swh/loader/svn/utils.py
@@ -3,7 +3,11 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import os
+
 from dateutil import parser
+
+from swh.model import git
 
 
 def strdate_to_timestamp(strdate):
@@ -51,5 +55,69 @@ def convert_hashes_with_relative_path(hashes, rootpath):
             v['children'] = _update_children(v['children'])
 
         h[p] = v
+
+    return h
+
+
+def hashtree(path, ignore_empty_folder, ignore=None):
+    """Given a path and options, compute the hash's upper tree.
+
+    This is not for production use.
+    It's merely a helper function used mainly in bin/hashtree.py
+
+    Args:
+        - path: The path to hash
+        - ignore_empty_folder: An option to ignore empty folder
+        - ignore: An option to ignore patterns in directory names.
+
+    Returns:
+        The path's checksums respecting the options passed as
+        parameters.
+
+    """
+    if os.path.exists(path):
+        if not os.path.isdir(path):
+            print('%s should be a directory!' % path)
+            return
+    else:
+        print('%s should exist!' % path)
+        return
+
+    if isinstance(path, str):
+        path = path.encode('utf-8')
+
+    if ignore:
+        patterns = []
+        for exc in ignore:
+            patterns.append(exc.encode('utf-8'))
+
+        def dir_ok_fn_basic(dirpath, patterns=patterns):
+            dname = os.path.basename(dirpath)
+            for pattern_to_ignore in patterns:
+                if pattern_to_ignore == dname:
+                    return False
+                if (pattern_to_ignore + b'/') in dirpath:
+                    return False
+            return True
+
+        if ignore_empty_folder:
+            def dir_ok_fn(dirpath, patterns=patterns):
+                if not dir_ok_fn_basic(dirpath):
+                    return False
+                return os.listdir(dirpath) != []
+        else:
+            dir_ok_fn = dir_ok_fn_basic
+    else:
+        if ignore_empty_folder:
+            def dir_ok_fn(dirpath):
+                return os.listdir(dirpath) != []
+        else:
+            dir_ok_fn = git.default_validation_dir
+
+    objects = git.compute_hashes_from_directory(
+        path,
+        dir_ok_fn=dir_ok_fn)
+
+    h = objects[path]['checksums']
 
     return h

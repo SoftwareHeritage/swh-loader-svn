e04dc9443938898a0e13be87b0c2e90bd3a3c90b
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-07-07 11:24:53 +0200

    Account for new svn commit that results in an empty folder use case
    Related r4ef17ff74e6c276ee6957e45f9bf2b2e7622b06a (swh-storage-testdata)

--- swh/loader/svn/tests/test_loader.py
+++ swh/loader/svn/tests/test_loader.py
@@ -357,17 +357,18 @@ class SWHSvnLoaderUpdateWithChangesITTest(BaseTestLoader):
         # then
         # we got the previous run's last revision (rev 6)
         # so 2 new
-        self.assertEquals(len(self.loader.all_revisions), 2)
+        self.assertEquals(len(self.loader.all_revisions), 3)
         self.assertEquals(len(self.loader.all_releases), 0)
         self.assertEquals(len(self.loader.all_occurrences), 1)
 
-        last_revision = '38d81702cb28db4f1a6821e64321e5825d1f7fd6'
+        last_revision = '99c27ebbd43feca179ac0e895af131d8314cafe1'
         # cf. test_loader.org for explaining from where those hashes
         # come from
         expected_revisions = {
             # revision hash | directory hash
             '7f5bc909c29d4e93d8ccfdda516e51ed44930ee1': '752c52134dcbf2fff13c7be1ce4e9e5dbf428a59',  # noqa
-            last_revision:                              '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            '38d81702cb28db4f1a6821e64321e5825d1f7fd6': '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            last_revision:                              '3397ca7f709639cbd36b18a0d1b70bce80018c45',  # noqa
         }
 
         for rev in self.loader.all_revisions:
@@ -420,17 +421,18 @@ class SWHSvnLoaderUpdateWithUnfinishedLoadingChangesITTest(BaseTestLoader):
         # then
         # we got the previous run's last revision (rev 6)
         # so 2 new
-        self.assertEquals(len(self.loader.all_revisions), 2)
+        self.assertEquals(len(self.loader.all_revisions), 3)
         self.assertEquals(len(self.loader.all_releases), 0)
         self.assertEquals(len(self.loader.all_occurrences), 1)
 
-        last_revision = '38d81702cb28db4f1a6821e64321e5825d1f7fd6'
+        last_revision = '99c27ebbd43feca179ac0e895af131d8314cafe1'
         # cf. test_loader.org for explaining from where those hashes
         # come from
         expected_revisions = {
             # revision hash | directory hash
             '7f5bc909c29d4e93d8ccfdda516e51ed44930ee1': '752c52134dcbf2fff13c7be1ce4e9e5dbf428a59',  # noqa
-            last_revision:                              '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            '38d81702cb28db4f1a6821e64321e5825d1f7fd6': '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            last_revision:                              '3397ca7f709639cbd36b18a0d1b70bce80018c45',  # noqa
         }
 
         for rev in self.loader.all_revisions:
@@ -485,17 +487,18 @@ class SWHSvnLoaderUpdateWithUnfinishedLoadingChangesButOccurrenceDoneITTest(
         # then
         # we got the previous run's last revision (rev 6)
         # so 2 new
-        self.assertEquals(len(self.loader.all_revisions), 2)
+        self.assertEquals(len(self.loader.all_revisions), 3)
         self.assertEquals(len(self.loader.all_releases), 0)
         self.assertEquals(len(self.loader.all_occurrences), 1)
 
-        last_revision = '38d81702cb28db4f1a6821e64321e5825d1f7fd6'
+        last_revision = '99c27ebbd43feca179ac0e895af131d8314cafe1'
         # cf. test_loader.org for explaining from where those hashes
         # come from
         expected_revisions = {
             # revision hash | directory hash
             '7f5bc909c29d4e93d8ccfdda516e51ed44930ee1': '752c52134dcbf2fff13c7be1ce4e9e5dbf428a59',  # noqa
-            last_revision:                              '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            '38d81702cb28db4f1a6821e64321e5825d1f7fd6': '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            last_revision:                              '3397ca7f709639cbd36b18a0d1b70bce80018c45',  # noqa
         }
 
         for rev in self.loader.all_revisions:
@@ -584,17 +587,18 @@ class SWHSvnLoaderUnfinishedLoadingChangesSinceLastOccurrenceITTest(
         # then
         # we got the previous run's last revision (rev 6)
         # so 2 new
-        self.assertEquals(len(self.loader.all_revisions), 2)
+        self.assertEquals(len(self.loader.all_revisions), 3)
         self.assertEquals(len(self.loader.all_releases), 0)
         self.assertEquals(len(self.loader.all_occurrences), 1)
 
-        last_revision = '38d81702cb28db4f1a6821e64321e5825d1f7fd6'
+        last_revision = '99c27ebbd43feca179ac0e895af131d8314cafe1'
         # cf. test_loader.org for explaining from where those hashes
         # come from
         expected_revisions = {
             # revision hash | directory hash
             '7f5bc909c29d4e93d8ccfdda516e51ed44930ee1': '752c52134dcbf2fff13c7be1ce4e9e5dbf428a59',  # noqa
-            last_revision:                              '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            '38d81702cb28db4f1a6821e64321e5825d1f7fd6': '39c813fb4717a4864bacefbd90b51a3241ae4140',  # noqa
+            last_revision:                              '3397ca7f709639cbd36b18a0d1b70bce80018c45',  # noqa
         }
 
         for rev in self.loader.all_revisions:

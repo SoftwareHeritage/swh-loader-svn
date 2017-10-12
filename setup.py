8cedf7417dea0b26edca3c6736939bde0a351e35
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-10-12 17:16:54 +0200

    Cleanup packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def parse_requirements():
@@ -19,7 +19,7 @@ setup(
     author='Software Heritage developers',
     author_email='swh-devel@inria.fr',
     url='https://forge.softwareheritage.org/diffusion/DLDSVN',
-    packages=['swh.loader.svn', 'swh.loader.svn.tests'],  # packages's modules
+    packages=find_packages(),  # packages's modules
     scripts=[],   # scripts to package
     install_requires=parse_requirements(),
     setup_requires=['vcversioner'],

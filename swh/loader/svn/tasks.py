0c8c40b88cd62c4f125d831b7bb42465450f734f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-03-08 15:18:54 +0100

    svn/tasks: Explicit tasks' arguments
    This avoids digging in code to understand how to trigger messages

--- swh/loader/svn/tasks.py
+++ swh/loader/svn/tasks.py
@@ -14,30 +14,42 @@ class LoadSWHSvnRepositoryTsk(Task):
     """
     task_queue = 'swh_loader_svn'
 
-    def run_task(self, *args, **kwargs):
+    def run_task(self, *, svn_url,
+                 destination_path=None,
+                 swh_revision=None,
+                 origin_url=None,
+                 visit_date=None,
+                 start_from_scratch=None):
         """Import a svn repository with swh policy.
 
         Args:
             args: ordered arguments (expected None)
             kwargs: Dictionary with the following expected keys:
 
-              - svn_url: (mandatory) svn's repository url
-              - destination_path: (mandatory) root directory to
+              - svn_url (str): (mandatory) svn's repository url
+              - destination_path (str): (mandatory) root directory to
                 locally retrieve svn's data
-              - swh_revision: (optional) extra SWH revision hex to
+              - origin_url (str): Optional original url override
+              - swh_revision (dict): (optional) extra SWH revision hex to
                 start from.  see swh.loader.svn.SvnLoader.process
                 docstring
 
         """
         loader = SWHSvnLoader()
         loader.log = self.log
-        return loader.load(*args, **kwargs)
+        return loader.load(
+            svn_url=svn_url,
+            destination_path=destination_path,
+            origin_url=origin_url,
+            swh_revision=swh_revision,
+            visit_date=visit_date,
+            start_from_scratch=start_from_scratch)
 
 
 class MountAndLoadSvnRepositoryTsk(Task):
     task_queue = 'swh_loader_svn_mount_and_load'
 
-    def run_task(self, archive_path, origin_url=None, visit_date=None,
+    def run_task(self, *, archive_path, origin_url=None, visit_date=None,
                  start_from_scratch=False):
         """1. Mount an svn dump from archive as a local svn repository.
            2. Load it through the svn loader.

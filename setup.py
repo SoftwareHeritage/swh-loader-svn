161f58254772c2072da27c35a19e55e483444fb2
David Douard <david.douard@sdfa3.org>
2018-10-05 14:17:30 +0200

    setup: prepare for pypi upload
    related to T1242

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-import os
+#!/usr/bin/env python3
+# Copyright (C) 2015-2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 from setuptools import setup, find_packages
+
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 def parse_requirements(name=None):
@@ -9,7 +23,7 @@ def parse_requirements(name=None):
         reqf = 'requirements.txt'
 
     requirements = []
-    if not os.path.exists(reqf):
+    if not path.exists(reqf):
         return requirements
 
     with open(reqf) as f:
@@ -24,14 +38,28 @@ def parse_requirements(name=None):
 setup(
     name='swh.loader.svn',
     description='Software Heritage Loader SVN',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Software Heritage developers',
     author_email='swh-devel@inria.fr',
     url='https://forge.softwareheritage.org/diffusion/DLDSVN',
-    packages=find_packages(),  # packages's modules
-    scripts=[],   # scripts to package
+    packages=find_packages(),
+    scripts=[],
     install_requires=parse_requirements() + parse_requirements('swh'),
     setup_requires=['vcversioner'],
     extras_require={'testing': parse_requirements('test')},
     vcversioner={},
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    project_urls={
+        'Bug Reports': 'https://forge.softwareheritage.org/maniphest',
+        'Funding': 'https://www.softwareheritage.org/donate',
+        'Source': 'https://forge.softwareheritage.org/source/swh-loader-svn',
+    },
 )

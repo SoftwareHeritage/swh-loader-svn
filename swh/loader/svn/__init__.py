ced8b2704f6e7a6bc9f529ee17c9cea70883f8a3
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-12-05 14:18:30 +0100

    loader.svn: Register svn worker
    Related to T2134

---
+++ swh/loader/svn/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from typing import Any, Dict
+
+
+def register() -> Dict[str, Any]:
+    from swh.loader.svn.loader import SvnLoader
+    return {
+        'task_modules': ['%s.tasks' % __name__],
+        'loader': SvnLoader,
+    }

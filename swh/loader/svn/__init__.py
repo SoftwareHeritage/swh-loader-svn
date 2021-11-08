d1678c538293e27ed489803e04587c58d2a01710
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-11-08 18:15:41 +0100

    svn: Modify default loader to use the same as production task
    Related to T3701#73419

--- swh/loader/svn/__init__.py
+++ swh/loader/svn/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019  The Software Heritage developers
+# Copyright (C) 2019-2021  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -7,9 +7,9 @@ from typing import Any, Dict
 
 
 def register() -> Dict[str, Any]:
-    from swh.loader.svn.loader import SvnLoader
+    from swh.loader.svn.loader import SvnLoaderFromRemoteDump
 
     return {
         "task_modules": ["%s.tasks" % __name__],
-        "loader": SvnLoader,
+        "loader": SvnLoaderFromRemoteDump,
     }

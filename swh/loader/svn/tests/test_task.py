5d3433e9cee155a72238dbd0748b65c2172973a8
Antoine Lambert <anlambert@softwareheritage.org>
2022-10-18 13:24:21 +0200

    tests: Simplify svn loading tasks creation tests implementation
    Use helper fixture loading_task_creation_for_listed_origin_test from
    swh-loader-core and remove redundant tests.

--- swh/loader/svn/tests/test_task.py
+++ swh/loader/svn/tests/test_task.py
@@ -8,12 +8,8 @@ import uuid
 import pytest
 
 from swh.scheduler.model import ListedOrigin, Lister
-from swh.scheduler.utils import create_origin_task_dict
 
-
-@pytest.fixture(autouse=True)
-def celery_worker_and_swh_config(swh_scheduler_celery_worker, swh_config):
-    pass
+NAMESPACE = "swh.loader.svn"
 
 
 @pytest.fixture
@@ -28,129 +24,55 @@ def svn_listed_origin(svn_lister):
     )
 
 
-@pytest.fixture
-def task_dict(svn_lister, svn_listed_origin):
-    return create_origin_task_dict(svn_listed_origin, svn_lister)
-
-
-def test_svn_loader(
-    mocker,
-    swh_scheduler_celery_app,
-):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoader.load")
-    mock_loader.return_value = {"status": "eventful"}
-
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.LoadSvnRepository",
-        kwargs=dict(
-            url="some-technical-url", origin_url="origin-url", visit_date="now"
-        ),
-    )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}
-
-
+@pytest.mark.parametrize("extra_loader_arguments", [{}, {"visit_date": "now"}])
 def test_svn_loader_for_listed_origin(
-    mocker,
-    swh_scheduler_celery_app,
-    task_dict,
-):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoader.load")
-    mock_loader.return_value = {"status": "eventful"}
-
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.LoadSvnRepository",
-        args=task_dict["arguments"]["args"],
-        kwargs=task_dict["arguments"]["kwargs"],
-    )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}
-
-
-def test_svn_loader_from_dump(
-    mocker,
-    swh_scheduler_celery_app,
-):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoaderFromDumpArchive.load")
-    mock_loader.return_value = {"status": "eventful"}
-
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.MountAndLoadSvnRepository",
-        kwargs=dict(url="some-url", archive_path="some-path", visit_date="now"),
-    )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}
-
-
-def test_svn_loader_from_dump_for_listed_origin(
-    mocker,
-    swh_scheduler_celery_app,
+    loading_task_creation_for_listed_origin_test,
     svn_lister,
     svn_listed_origin,
+    extra_loader_arguments,
 ):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoaderFromDumpArchive.load")
-    mock_loader.return_value = {"status": "eventful"}
+    svn_listed_origin.extra_loader_arguments = extra_loader_arguments
 
-    svn_listed_origin.extra_loader_arguments = {"archive_path": "some-path"}
-
-    task_dict = create_origin_task_dict(svn_listed_origin, svn_lister)
-
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.MountAndLoadSvnRepository",
-        args=task_dict["arguments"]["args"],
-        kwargs=task_dict["arguments"]["kwargs"],
+    loading_task_creation_for_listed_origin_test(
+        loader_class_name=f"{NAMESPACE}.loader.SvnLoader",
+        task_function_name=f"{NAMESPACE}.tasks.LoadSvnRepository",
+        lister=svn_lister,
+        listed_origin=svn_listed_origin,
     )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}
 
 
-def test_svn_loader_from_remote_dump(
-    mocker,
-    swh_scheduler_celery_app,
+@pytest.mark.parametrize(
+    "extra_loader_arguments",
+    [{"archive_path": "some-path"}, {"archive_path": "some-path", "visit_date": "now"}],
+)
+def test_svn_loader_from_dump_for_listed_origin(
+    loading_task_creation_for_listed_origin_test,
+    svn_lister,
+    svn_listed_origin,
+    extra_loader_arguments,
 ):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoaderFromRemoteDump.load")
-    mock_loader.return_value = {"status": "eventful"}
+    svn_listed_origin.extra_loader_arguments = extra_loader_arguments
 
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.DumpMountAndLoadSvnRepository",
-        kwargs=dict(
-            url="some-remote-dump-url", origin_url="origin-url", visit_date="now"
-        ),
+    loading_task_creation_for_listed_origin_test(
+        loader_class_name=f"{NAMESPACE}.loader.SvnLoaderFromDumpArchive",
+        task_function_name=f"{NAMESPACE}.tasks.MountAndLoadSvnRepository",
+        lister=svn_lister,
+        listed_origin=svn_listed_origin,
     )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}
 
 
+@pytest.mark.parametrize("extra_loader_arguments", [{}, {"visit_date": "now"}])
 def test_svn_loader_from_remote_dump_for_listed_origin(
-    mocker,
-    swh_scheduler_celery_app,
-    task_dict,
+    loading_task_creation_for_listed_origin_test,
+    svn_lister,
+    svn_listed_origin,
+    extra_loader_arguments,
 ):
-    mock_loader = mocker.patch("swh.loader.svn.loader.SvnLoaderFromRemoteDump.load")
-    mock_loader.return_value = {"status": "eventful"}
+    svn_listed_origin.extra_loader_arguments = extra_loader_arguments
 
-    res = swh_scheduler_celery_app.send_task(
-        "swh.loader.svn.tasks.DumpMountAndLoadSvnRepository",
-        args=task_dict["arguments"]["args"],
-        kwargs=task_dict["arguments"]["kwargs"],
+    loading_task_creation_for_listed_origin_test(
+        loader_class_name=f"{NAMESPACE}.loader.SvnLoaderFromRemoteDump",
+        task_function_name=f"{NAMESPACE}.tasks.DumpMountAndLoadSvnRepository",
+        lister=svn_lister,
+        listed_origin=svn_listed_origin,
     )
-    assert res
-    res.wait()
-    assert res.successful()
-
-    assert res.result == {"status": "eventful"}

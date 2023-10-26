SoftwareHeritage/swh-loader-svn

commit c834321d77edb6dbaa7cbbe50505a6468f04ffdc

apply isort globally

swh/loader/svn/converters.py
@@ -7,7 +7,6 @@ import datetime
 from typing import Dict, Optional, Sequence, Tuple
 
 import iso8601
-
 from swh.model.model import Person, Revision, RevisionType, TimestampWithTimezone
 
 

swh/loader/svn/directory.py
@@ -11,8 +11,9 @@ from pathlib import Path
 from typing import Iterator, Optional
 
 from swh.loader.core.loader import BaseDirectoryLoader
-from swh.loader.svn.svn_repo import SvnRepo, get_svn_repo
 from swh.model.model import Snapshot, SnapshotBranch, TargetType
+
+from swh.loader.svn.svn_repo import SvnRepo, get_svn_repo
 
 
 class SvnExportLoader(BaseDirectoryLoader):

swh/loader/svn/loader.py
@@ -20,7 +20,6 @@ from typing import Any, Dict, Iterator, List, Optional, Sequence, Tuple
 from swh.loader.core.loader import BaseLoader
 from swh.loader.core.utils import clean_dangling_folders
 from swh.loader.exception import NotFound
-from swh.loader.svn.svn_repo import get_svn_repo
 from swh.model import from_disk, hashutil
 from swh.model.model import (
     Content,
@@ -33,6 +32,8 @@ from swh.model.model import (
 )
 from swh.storage.algos.snapshot import snapshot_get_latest
 from swh.storage.interface import StorageInterface
+
+from swh.loader.svn.svn_repo import get_svn_repo
 
 from . import converters
 from .exception import SvnLoaderHistoryAltered, SvnLoaderUneventful

swh/loader/svn/replay.py
@@ -36,7 +36,6 @@ from typing import (
 import click
 from subvertpy import SubversionException, properties
 from subvertpy.ra import Auth, RemoteAccess, get_username_provider
-
 from swh.model import from_disk, hashutil
 from swh.model.from_disk import DiskBackedContent
 from swh.model.model import Content, Directory, SkippedContent

swh/loader/svn/svn_repo.py
@@ -25,7 +25,6 @@ from subvertpy.ra import (
     get_simple_prompt_provider,
     get_username_provider,
 )
-
 from swh.loader.exception import NotFound
 from swh.model.from_disk import Directory as DirectoryFromDisk
 from swh.model.model import Content, Directory, SkippedContent

swh/loader/svn/tasks.py
@@ -5,7 +5,6 @@
 
 
 from celery import shared_task
-
 from swh.loader.core.utils import parse_visit_date
 
 from .directory import SvnExportLoader

swh/loader/svn/tests/conftest.py
@@ -8,10 +8,10 @@ from typing import Any, Dict
 import uuid
 
 import pytest
+from swh.scheduler.model import Lister
 
 from swh.loader.svn.loader import SvnLoader, SvnLoaderFromRemoteDump
 from swh.loader.svn.svn_repo import SvnRepo
-from swh.scheduler.model import Lister
 
 from .utils import create_repo
 

swh/loader/svn/tests/test_converters.py
@@ -3,9 +3,10 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from swh.loader.svn import converters
 from swh.model.hashutil import hash_to_bytes
 from swh.model.model import Person, Revision, Timestamp, TimestampWithTimezone
+
+from swh.loader.svn import converters
 
 
 def test_svn_author_to_swh_person():

commit 70907645f358594240a1247cc4e4d038447a4911

Fix isort formatting
It was broken in c834321d77edb6dbaa7cbbe50505a6468f04ffdc likely
due to a side effect related to a local configuration.

swh/loader/svn/converters.py
@@ -7,6 +7,7 @@ import datetime
 from typing import Dict, Optional, Sequence, Tuple
 
 import iso8601
+
 from swh.model.model import Person, Revision, RevisionType, TimestampWithTimezone
 
 

swh/loader/svn/directory.py
@@ -11,9 +11,8 @@ from pathlib import Path
 from typing import Iterator, Optional
 
 from swh.loader.core.loader import BaseDirectoryLoader
-from swh.model.model import Snapshot, SnapshotBranch, TargetType
-
 from swh.loader.svn.svn_repo import SvnRepo, get_svn_repo
+from swh.model.model import Snapshot, SnapshotBranch, TargetType
 
 
 class SvnExportLoader(BaseDirectoryLoader):

swh/loader/svn/loader.py
@@ -20,6 +20,7 @@ from typing import Any, Dict, Iterator, List, Optional, Sequence, Tuple
 from swh.loader.core.loader import BaseLoader
 from swh.loader.core.utils import clean_dangling_folders
 from swh.loader.exception import NotFound
+from swh.loader.svn.svn_repo import get_svn_repo
 from swh.model import from_disk, hashutil
 from swh.model.model import (
     Content,
@@ -32,8 +33,6 @@ from swh.model.model import (
 )
 from swh.storage.algos.snapshot import snapshot_get_latest
 from swh.storage.interface import StorageInterface
-
-from swh.loader.svn.svn_repo import get_svn_repo
 
 from . import converters
 from .exception import SvnLoaderHistoryAltered, SvnLoaderUneventful

swh/loader/svn/replay.py
@@ -36,6 +36,7 @@ from typing import (
 import click
 from subvertpy import SubversionException, properties
 from subvertpy.ra import Auth, RemoteAccess, get_username_provider
+
 from swh.model import from_disk, hashutil
 from swh.model.from_disk import DiskBackedContent
 from swh.model.model import Content, Directory, SkippedContent

swh/loader/svn/svn_repo.py
@@ -25,6 +25,7 @@ from subvertpy.ra import (
     get_simple_prompt_provider,
     get_username_provider,
 )
+
 from swh.loader.exception import NotFound
 from swh.model.from_disk import Directory as DirectoryFromDisk
 from swh.model.model import Content, Directory, SkippedContent

swh/loader/svn/tasks.py
@@ -5,6 +5,7 @@
 
 
 from celery import shared_task
+
 from swh.loader.core.utils import parse_visit_date
 
 from .directory import SvnExportLoader

swh/loader/svn/tests/conftest.py
@@ -8,10 +8,10 @@ from typing import Any, Dict
 import uuid
 
 import pytest
-from swh.scheduler.model import Lister
 
 from swh.loader.svn.loader import SvnLoader, SvnLoaderFromRemoteDump
 from swh.loader.svn.svn_repo import SvnRepo
+from swh.scheduler.model import Lister
 
 from .utils import create_repo
 

swh/loader/svn/tests/test_converters.py
@@ -3,10 +3,9 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from swh.loader.svn import converters
 from swh.model.hashutil import hash_to_bytes
 from swh.model.model import Person, Revision, Timestamp, TimestampWithTimezone
-
-from swh.loader.svn import converters
 
 
 def test_svn_author_to_swh_person():

swh/loader/svn/tests/test_directory.py
@@ -6,15 +6,14 @@
 import os
 
 from swh.loader.core.nar import Nar
+from swh.loader.svn.directory import SvnExportLoader
+from swh.loader.svn.svn_repo import get_svn_repo
 from swh.loader.tests import (
     assert_last_visit_matches,
     fetch_nar_extids_from_checksums,
     get_stats,
     prepare_repository_from_archive,
 )
-
-from swh.loader.svn.directory import SvnExportLoader
-from swh.loader.svn.svn_repo import get_svn_repo
 
 
 def compute_nar_hash_for_rev(repo_url: str, rev: int, hash_name: str = "sha256") -> str:

swh/loader/svn/tests/test_externals.py
@@ -6,10 +6,10 @@
 from datetime import datetime, timedelta, timezone
 
 import pytest
-from swh.loader.tests import assert_last_visit_matches, check_snapshot
 
 from swh.loader.svn.loader import SvnLoader, SvnLoaderFromRemoteDump
 from swh.loader.svn.utils import svn_urljoin
+from swh.loader.tests import assert_last_visit_matches, check_snapshot
 
 from .utils import CommitChange, CommitChangeType, add_commit, create_repo
 

swh/loader/svn/tests/test_loader.py
@@ -13,6 +13,14 @@ from typing import Any, Dict
 
 import pytest
 from subvertpy import SubversionException
+
+from swh.loader.svn.loader import (
+    SvnLoader,
+    SvnLoaderFromDumpArchive,
+    SvnLoaderFromRemoteDump,
+)
+from swh.loader.svn.svn_repo import SvnRepo
+from swh.loader.svn.utils import init_svn_repo_from_dump
 from swh.loader.tests import (
     assert_last_visit_matches,
     check_snapshot,
@@ -22,14 +30,6 @@ from swh.loader.tests import (
 from swh.model.from_disk import DentryPerms, Directory
 from swh.model.hashutil import hash_to_bytes
 from swh.model.model import Snapshot, SnapshotBranch, TargetType
-
-from swh.loader.svn.loader import (
-    SvnLoader,
-    SvnLoaderFromDumpArchive,
-    SvnLoaderFromRemoteDump,
-)
-from swh.loader.svn.svn_repo import SvnRepo
-from swh.loader.svn.utils import init_svn_repo_from_dump
 
 from .utils import CommitChange, CommitChangeType, add_commit
 

swh/loader/svn/tests/test_svn_retry.py
@@ -8,10 +8,10 @@ import os
 import pytest
 from subvertpy import SubversionException
 from subvertpy.ra import Auth, RemoteAccess, get_username_provider
-from swh.loader.tests import prepare_repository_from_archive
 
 from swh.loader.svn.svn_repo import SvnRepo
 from swh.loader.svn.svn_retry import SVN_RETRY_MAX_ATTEMPTS, SVN_RETRY_WAIT_EXP_BASE
+from swh.loader.tests import prepare_repository_from_archive
 
 
 def _get_repo_url(archive_name, datadir, tmp_path):

swh/loader/svn/tests/test_task_directory.py
@@ -6,6 +6,7 @@
 from typing import Any, Dict
 
 import pytest
+
 from swh.scheduler.model import ListedOrigin
 
 from .conftest import NAMESPACE

swh/loader/svn/tests/test_utils.py
@@ -13,9 +13,9 @@ import shutil
 from subprocess import Popen, run
 
 import pytest
-from swh.loader.tests import prepare_repository_from_archive
 
 from swh.loader.svn import utils
+from swh.loader.tests import prepare_repository_from_archive
 
 from .utils import CommitChange, CommitChangeType, add_commit
 
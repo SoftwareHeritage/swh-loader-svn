SoftwareHeritage/swh-loader-svn

commit f94f1d0ef41d6c8e638337601a29f3352a478979

loader: Prevent errors when loading repository sub-path from a dump
When loading a sub-path of a repository from a dump file it has been
observed it is less error-prone to dump the whole repository as some
partial dumps fail to be loaded by svnadmin or svnrdump can end up
with error.

In that case, we try to produce a dump file for the whole repository
and we stop to dump revisions once the last one modifying the repository
sub-path was dumped (revisions not modifying sub-path are then filtered
out during the loading process).

swh/loader/svn/loader.py
@@ -759,7 +759,7 @@ class SvnLoaderFromRemoteDump(SvnLoader):
         return svn_revision
 
     def dump_svn_revisions(
-        self, svn_url: str, last_loaded_svn_rev: int = -1
+        self, svn_url: str, max_rev: int = -1, last_loaded_svn_rev: int = -1
     ) -> Tuple[str, int]:
         """Generate a compressed subversion dump file using the svnrdump tool and gzip.
         If the svnrdump command failed somehow, the produced dump file is analyzed to
@@ -782,6 +782,8 @@ class SvnLoaderFromRemoteDump(SvnLoader):
                 "--password",
                 self.svnrepo.password,
             ]
+        if max_rev > 0:
+            svnrdump_cmd.append(f"-r0:{max_rev}")
 
         # Launch the svnrdump command while capturing stderr as
         # successfully dumped revision numbers are printed to it
@@ -876,6 +878,26 @@ class SvnLoaderFromRemoteDump(SvnLoader):
         # and svnrdump does not handle URL redirection
         self.svn_url = self.svnrepo.remote_url
 
+        max_rev = -1
+        if self.svnrepo.root_directory:
+            # When loading a sub-path of a repository from a dump file it has been
+            # observed it is less error-prone to dump the whole repository as some
+            # partial dumps fail to be loaded by svnadmin or svnrdump can end up
+            # with error
+            try:
+                self.svnrepo.info(self.svnrepo.repos_root_url)
+            except SubversionException:
+                # Repository root URL cannot be accessed by a svn client, try to
+                # load from a partial dump then
+                pass
+            else:
+                # A dump file for the whole repository can be produced, in that case
+                # we stop to dump revisions once the last one modifying the repository
+                # sub-path was dumped (revisions not modifying sub-path are then filtered
+                # out during the loading process).
+                max_rev = self.svnrepo.info(self.svn_url).last_changed_revision
+                self.svn_url = self.svnrepo.repos_root_url
+
         # Then for stale repository, check if the last loaded revision in the archive
         # is different from the last revision on the remote subversion server.
         # Skip the dump of all revisions and the loading process if they are identical
@@ -895,7 +917,9 @@ class SvnLoaderFromRemoteDump(SvnLoader):
 
         # Then try to generate a dump file containing relevant svn revisions
         # to load, an exception will be thrown if something wrong happened
-        dump_path, max_rev = self.dump_svn_revisions(self.svn_url, last_loaded_svn_rev)
+        dump_path, max_rev = self.dump_svn_revisions(
+            self.svn_url, max_rev, last_loaded_svn_rev
+        )
 
         # Finally, mount the dump and load the repository
         self.log.debug('Mounting dump file with "svnadmin load".')

swh/loader/svn/tests/test_loader.py
@@ -2000,7 +2000,7 @@ def test_loader_with_subprojects(
         assert len(root_dir) == 1 and root_dir[0]["type"] == "file"
 
         if svn_loader_cls == SvnLoaderFromRemoteDump:
-            dump_revisions.assert_called_once_with(origin_url.rstrip("/"), -1)
+            dump_revisions.assert_called_once_with(repo_url, i, -1)
 
         if svn_loader_cls == SvnLoaderFromDumpArchive:
             loader_params["archive_path"] = _dump_project(tmp_path, origin_url)

commit e74a73435be126f8863424bdd98be7eb23c067f7

loader: Use proper way to get latest revision where a path was modified
This fixes a regression introduced in f94f1d0.

Using the svn info command to get the latest revision where a path was
modified is not reliable as it does not handle the case where a path was
copied from a previous revision. In that case, the info command is returning
the latest revision where the path was modified before the copy. So the
loading of a repository subpath that was copied could not succeed as the
revision where the copy happened was not considered.

Instead use the svn log command that ensures the revision where the copy
of the path happened is returned.

swh/loader/svn/loader.py
@@ -912,7 +912,25 @@ class SvnLoaderFromRemoteDump(SvnLoader):
                 # we stop to dump revisions once the last one modifying the repository
                 # sub-path was dumped (revisions not modifying sub-path are then filtered
                 # out during the loading process).
-                max_rev = self.svnrepo.info(self.svn_url).last_changed_revision
+
+                path = self.svnrepo.remote_url.replace(
+                    self.svnrepo.repos_root_url + "/", ""
+                )
+                svn_repo = self.svn_repo(
+                    self.svnrepo.repos_root_url,
+                    self.svnrepo.repos_root_url,
+                    self.temp_dir,
+                )
+                path_last_log = list(
+                    svn_repo.logs(
+                        svn_repo.head_revision(),
+                        0,
+                        paths=[path],
+                        discover_changed_paths=False,
+                        limit=1,
+                    )
+                )
+                max_rev = path_last_log[0]["rev"]
                 self.svn_url = self.svnrepo.repos_root_url
 
         # Then for stale repository, check if the last loaded revision in the archive

swh/loader/svn/svn_repo.py
@@ -208,8 +208,11 @@ class SvnRepo:
         message = revprops.get(properties.PROP_REVISION_LOG, DEFAULT_AUTHOR_MESSAGE)
 
         has_changes = changed_paths is not None and any(
-            changed_path.startswith(self.root_directory)
-            for changed_path in changed_paths.keys()
+            (
+                changed_path.startswith(self.root_directory)
+                or (copyfrom_rev != -1 and self.root_directory.startswith(changed_path))
+            )
+            for changed_path, (_, _, copyfrom_rev, _) in changed_paths.items()
         )
 
         return {
@@ -225,6 +228,9 @@ class SvnRepo:
         self,
         revision_start: int,
         revision_end: int,
+        paths: Optional[List[str]] = None,
+        limit: int = 0,
+        discover_changed_paths: bool = True,
     ) -> Iterator[Dict]:
         """Stream svn logs between revision_start and revision_end.
 
@@ -247,10 +253,11 @@ class SvnRepo:
 
         """
         for log_entry in self.remote_access().iter_log(
-            paths=None,
+            paths=paths,
             start=revision_start,
             end=revision_end,
-            discover_changed_paths=True,
+            discover_changed_paths=discover_changed_paths,
+            limit=limit,
         ):
             yield self._revision_data(log_entry)
 

swh/loader/svn/tests/test_loader.py
@@ -2554,3 +2554,39 @@ def test_loader_svnrepo_propget_on_url(svn_loader_cls, swh_storage, repo_url, tm
     }
 
     assert loader.svnrepo.propget("svn:eol-style", baz_file_url, peg_rev=1, rev=1) == {}
+
+
+def test_loader_subpath_from_copy(swh_storage, repo_url, tmp_path, svn_loader_cls):
+    add_commit(
+        repo_url,
+        "Create trunk/data/foo file",
+        [
+            CommitChange(
+                change_type=CommitChangeType.AddOrUpdate,
+                path="trunk/data/foo",
+                data=b"foo",
+            ),
+        ],
+    )
+
+    add_commit(
+        repo_url,
+        "Copy trunk/ folder from revision 1",
+        [
+            CommitChange(
+                change_type=CommitChangeType.AddOrUpdate,
+                path="tags/v1/",
+                copyfrom_path=repo_url + "/trunk/",
+                copyfrom_rev=1,
+            ),
+        ],
+    )
+
+    loader = svn_loader_cls(
+        swh_storage,
+        repo_url + "/tags/v1/data",
+        temp_directory=tmp_path,
+        debug=True,
+        check_revision=1,
+    )
+    assert loader.load() == {"status": "eventful"}